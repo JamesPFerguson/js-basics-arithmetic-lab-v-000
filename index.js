ef7361a0191058d30735e588f65b7554463bb493
James Ferguson <ncjamesferguson@gmail.com>
2018-02-28 05:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const randomNumber = Math.floor(Math.random() * 20)
 
 const randomInteger = Math.floor(randomNumber)
 
-const randomUserID = randomInteger + 100000000
+const randomUserID = randomInteger + 1000000000
